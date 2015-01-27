9d9901dfdb2f215fdb1d6a28d7cdfe85e1cc57da
Alex Bate <alex@alexbate.co.uk>
2015-01-27 16:17:24 +0000

    Setup project and print init message to console

---
+++ src/uk/ac/cam/cl/bravo/PlanetBuilder/MainClass.java
@@ -0,0 +1,9 @@
+package uk.ac.cam.cl.bravo.PlanetBuilder;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("Planet Builder launched");
+    }
+
+}
