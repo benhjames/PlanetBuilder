5db27a0e1c728a46b78f49d8086305d9c5da5390
Alex Bate <alex@alexbate.co.uk>
2015-01-29 17:35:05 +0000

    Add variables and basic getters and setters

--- src/uk/ac/cam/cl/bravo/PlanetBuilder/GlobalOptions.java
+++ src/uk/ac/cam/cl/bravo/PlanetBuilder/GlobalOptions.java
@@ -3,8 +3,13 @@ package uk.ac.cam.cl.bravo.PlanetBuilder;
 public class GlobalOptions {
     private static GlobalOptions go = null;
 
+    private float detailLevel;
+    private boolean renderHigh;
+
     protected GlobalOptions() {
         //Initialise options with defaults
+        detailLevel = 50.0f;
+        renderHigh = true;
     }
 
     public GlobalOptions getInstance() {
@@ -13,4 +18,25 @@ public class GlobalOptions {
         }
         return go;
     }
+
+    //Getters and setters only below
+
+    public float getDetailLevel() {
+        return detailLevel;
+    }
+
+    public void setDetailLevel(float detailLevel) {
+        if (detailLevel >= 0.0f && detailLevel <= 100.0f) {
+            this.detailLevel = detailLevel;
+        }
+    }
+
+    public boolean isRenderHigh() {
+        return renderHigh;
+    }
+
+    public void setRenderHigh(boolean renderHigh) {
+        this.renderHigh = renderHigh;
+    }
+
 }
