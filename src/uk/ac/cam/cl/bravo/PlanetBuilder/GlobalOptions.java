0995d491e5f45cd1fded53f1c935d89f0756fdb5
Alex Bate <alex@alexbate.co.uk>
2015-01-29 17:23:55 +0000

    Add singleton pattern to GlobalOptions

--- src/uk/ac/cam/cl/bravo/PlanetBuilder/GlobalOptions.java
+++ src/uk/ac/cam/cl/bravo/PlanetBuilder/GlobalOptions.java
@@ -1,5 +1,16 @@
 package uk.ac.cam.cl.bravo.PlanetBuilder;
 
 public class GlobalOptions {
+    private static GlobalOptions go = null;
 
+    protected GlobalOptions() {
+        //Initialise options with defaults
+    }
+
+    public GlobalOptions getInstance() {
+        if (go == null) {
+            go = new GlobalOptions();
+        }
+        return go;
+    }
 }
