fa0e1bc99be492e00bd33cf4f0219b7d74c392d8
Sam---Wang <xw302@cam.ac.uk>
2015-02-01 18:48:43 +0000

    Create IndexedMesh.java
    Class that holds a mesh's data using indices

---
+++ IndexedMesh.java
@@ -0,0 +1,23 @@
+package xw302;
+
+/**
+ * Class that holds a mesh's data using indices
+ * 
+ */
+public class IndexedMesh extends Mesh {
+        
+        /**
+         * Indices of this IndexedMesh
+         */
+        protected int[] indices;
+        
+        /**
+         * Returns the indices for this IndexedMesh.
+         * 
+         * @return the indices
+         */
+        public int[] getIndices() {
+                return indices;
+        }
+        
+}
