35a603104bc4bd66751e25d677e2628ff34d3e93
Alex Bate <alex@alexbate.co.uk>
2015-02-19 14:24:31 +0000

    Tweak Leap Motion control

--- src/uk/ac/cam/cl/bravo/PlanetBuilder/Leap.java
+++ src/uk/ac/cam/cl/bravo/PlanetBuilder/Leap.java
@@ -12,8 +12,9 @@ public class Leap {
     private static Frame lastFrame = controller.frame();
 
     private static final float translationProb = 0.5f;
-    private static final float translationMulti = 0.3f;
-    private static final float zoomMulti = 0.3f;
+    private static final float rotationProb = 0.5f;
+    private static final float translationMulti = 0.7f;
+    private static final float zoomMulti = 0.2f;
 
     public static void setWindow(MainWindow mainWindow) {
         window = mainWindow;
@@ -22,17 +23,24 @@ public class Leap {
     public static void updateLeapControls() {
         if (controller.isConnected()) {
             Frame thisFrame = controller.frame();
+            double xTheta = 0;
+            double yTheta = 0;
 
             Vector translation = thisFrame.translation(lastFrame);
             if (thisFrame.translationProbability(lastFrame) > translationProb) {
-                double xTheta = translation.getX() * translationMulti;
-                double yTheta = -1 * translation.getZ() * translationMulti; //Our and leap's co'ord systems are different
-                window.getCamera().circle(xTheta, yTheta);
+                xTheta = -1 * translation.getX() * translationMulti;
+                yTheta = -1 * translation.getZ() * translationMulti; //Our and leap's co'ord systems are different
 
                 float zoom = translation.getY() * zoomMulti;
                 window.getCamera().zoom(zoom);
             }
 
+            if (thisFrame.rotationProbability(lastFrame) > rotationProb) {
+                xTheta += thisFrame.rotationAngle(lastFrame, Vector.xAxis());
+                yTheta -= thisFrame.rotationAngle(lastFrame, Vector.zAxis());
+            }
+
+            window.getCamera().circle(xTheta, yTheta);
             lastFrame = thisFrame;
         }
     }
