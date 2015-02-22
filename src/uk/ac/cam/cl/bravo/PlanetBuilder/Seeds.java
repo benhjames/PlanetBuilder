9e91a1853925320050d1a326bece68d3800b6e45
Ben James <benhjames@sky.com>
2015-02-22 21:44:50 +0000

    Increase fillings sophistication (amendment: remove .iml file)

--- src/uk/ac/cam/cl/bravo/PlanetBuilder/Seeds.java
+++ src/uk/ac/cam/cl/bravo/PlanetBuilder/Seeds.java
@@ -4,4 +4,5 @@ public class Seeds {
 	public final static int FillingTypeSeed = 0;
 	public final static int HeightSeed = 1;
 	public final static int ClimateSeed = 2;
+	public final static int FillingSettlementSeed = 3;
 }
