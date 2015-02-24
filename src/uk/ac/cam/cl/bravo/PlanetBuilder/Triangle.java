cda40b9175e88d13384c8ca19c18d12673464451
Alfie Wallace <alfie+git@alfiewallace.com>
2015-02-24 23:45:44 +0000

    Fixed settlements not adjusting with terrain

--- src/uk/ac/cam/cl/bravo/PlanetBuilder/Triangle.java
+++ src/uk/ac/cam/cl/bravo/PlanetBuilder/Triangle.java
@@ -113,53 +113,51 @@ class Triangle {
 			c2 = interpolate(settlementColor1, settlementColor2, v2.getClimateNoise());
 			c3 = interpolate(settlementColor1, settlementColor2, v3.getClimateNoise());
 
-            if (models == null) {
-                float[] modelsPos = getmidnormal();
+            float[] modelsPos = getmidnormal();
 
-                Vec4 pos = new Vec4(modelsPos[0], modelsPos[1], modelsPos[2], 1.0f);
-                Vec3 dir = new Vec3(modelsPos[3], modelsPos[4], modelsPos[5]);
+            Vec4 pos = new Vec4(modelsPos[0], modelsPos[1], modelsPos[2], 1.0f);
+            Vec3 dir = new Vec3(modelsPos[3], modelsPos[4], modelsPos[5]);
 
-                Vec3 rotationDir = dir.cross(new Vec3(0,1,0)).getUnitVector();
-                float angle = -dir.angleInRadians(new Vec3(0,1,0));
-                float rndAngle = (float)((new Random()).nextInt(90) * 0.0174532925);
+            Vec3 rotationDir = dir.cross(new Vec3(0,1,0)).getUnitVector();
+            float angle = -dir.angleInRadians(new Vec3(0,1,0));
+            float rndAngle = (float)((new Random()).nextInt(90) * 0.0174532925);
 
-                Mat4 rotationMat = Matrices.rotate(angle, rotationDir);
-                Mat4 rotationMat2 = Matrices.rotate(rndAngle, dir);
-                rotationMat = rotationMat2.multiply(rotationMat);
-                Mat4 translationMat = new Mat4((Vec4)rotationMat.getColumn(0), (Vec4)rotationMat.getColumn(1), (Vec4)rotationMat.getColumn(2), pos);
+            Mat4 rotationMat = Matrices.rotate(angle, rotationDir);
+            Mat4 rotationMat2 = Matrices.rotate(rndAngle, dir);
+            rotationMat = rotationMat2.multiply(rotationMat);
+            Mat4 translationMat = new Mat4((Vec4)rotationMat.getColumn(0), (Vec4)rotationMat.getColumn(1), (Vec4)rotationMat.getColumn(2), pos);
 
-                int vertexCount = StructureModel.getVertexArray().length / 3;
-                models = new float[vertexCount * 3];
-                modelsColors = new float[vertexCount * 4];
+            int vertexCount = StructureModel.getVertexArray().length / 3;
+            models = new float[vertexCount * 3];
+            modelsColors = new float[vertexCount * 4];
 
-                float scale = 1f/(float)Math.pow(2f, GlobalOptions.getInstance().getDetailLevel() + 2f);
-                float heightScale = 1.0f + (new Random()).nextFloat();
-                float rndColor = (new Random()).nextFloat() * 15f / 100f;
+            float scale = 1f/(float)Math.pow(2f, GlobalOptions.getInstance().getDetailLevel() + 2f);
+            float heightScale = 1.0f + (new Random()).nextFloat();
+            float rndColor = (new Random()).nextFloat() * 15f / 100f;
 
-                for (int i = 0; i < vertexCount; i++) {
-                    float x = StructureModel.getVertexArray()[i*3 + 0] * scale;
-                    float y = StructureModel.getVertexArray()[i*3 + 1] * scale * heightScale;
-                    float z = StructureModel.getVertexArray()[i*3 + 2] * scale;
-                    float w = 1f;
+            for (int i = 0; i < vertexCount; i++) {
+                float x = StructureModel.getVertexArray()[i*3 + 0] * scale;
+                float y = StructureModel.getVertexArray()[i*3 + 1] * scale * heightScale;
+                float z = StructureModel.getVertexArray()[i*3 + 2] * scale;
+                float w = 1f;
 
-                    Vec4 col0 = translationMat.getColumn(0);
-                    Vec4 col1 = translationMat.getColumn(1);
-                    Vec4 col2 = translationMat.getColumn(2);
-                    Vec4 col3 = translationMat.getColumn(3);
+                Vec4 col0 = translationMat.getColumn(0);
+                Vec4 col1 = translationMat.getColumn(1);
+                Vec4 col2 = translationMat.getColumn(2);
+                Vec4 col3 = translationMat.getColumn(3);
 
-                    float newX = (col0.getX()*x + col1.getX()*y + col2.getX()*z + col3.getX()*w);
-                    float newY = (col0.getY()*x + col1.getY()*y + col2.getY()*z + col3.getY()*w);
-                    float newZ = (col0.getZ()*x + col1.getZ()*y + col2.getZ()*z + col3.getZ()*w);
+                float newX = (col0.getX()*x + col1.getX()*y + col2.getX()*z + col3.getX()*w);
+                float newY = (col0.getY()*x + col1.getY()*y + col2.getY()*z + col3.getY()*w);
+                float newZ = (col0.getZ()*x + col1.getZ()*y + col2.getZ()*z + col3.getZ()*w);
 
-                    models[i*3 + 0] = newX;
-                    models[i*3 + 1] = newY;
-                    models[i*3 + 2] = newZ;
+                models[i*3 + 0] = newX;
+                models[i*3 + 1] = newY;
+                models[i*3 + 2] = newZ;
 
-                    modelsColors[i*4 + 0] = 0.5f + rndColor;
-                    modelsColors[i*4 + 1] = 0.5f + rndColor;
-                    modelsColors[i*4 + 2] = 0.5f + rndColor;
-                    modelsColors[i*4 + 3] = 1.0f;
-                }
+                modelsColors[i*4 + 0] = 0.5f + rndColor;
+                modelsColors[i*4 + 1] = 0.5f + rndColor;
+                modelsColors[i*4 + 2] = 0.5f + rndColor;
+                modelsColors[i*4 + 3] = 1.0f;
             }
             
 		} else if (fillingTypeNoise < desertBound && isAboveSea()) {
@@ -184,56 +182,52 @@ class Triangle {
 			c2 = interpolate(vegColor1, vegColor2, v2.getClimateNoise());
 			c3 = interpolate(vegColor1, vegColor2, v3.getClimateNoise());
 
+            float[] modelsPos = getmidnormal();
 
+            Vec4 pos = new Vec4(modelsPos[0], modelsPos[1], modelsPos[2], 1.0f);
+            Vec3 dir = new Vec3(modelsPos[3], modelsPos[4], modelsPos[5]);
 
-            if (models == null) {
-                float[] modelsPos = getmidnormal();
+            Vec3 rotationDir = dir.cross(new Vec3(0,1,0)).getUnitVector();
+            float angle = -dir.angleInRadians(new Vec3(0,1,0));
+            float rndAngle = (float)((new Random()).nextInt(90) * 0.0174532925);
 
-                Vec4 pos = new Vec4(modelsPos[0], modelsPos[1], modelsPos[2], 1.0f);
-                Vec3 dir = new Vec3(modelsPos[3], modelsPos[4], modelsPos[5]);
+            Mat4 rotationMat = Matrices.rotate(angle, rotationDir);
+            Mat4 rotationMat2 = Matrices.rotate(rndAngle, dir);
+            rotationMat = rotationMat2.multiply(rotationMat);
+            Mat4 translationMat = new Mat4((Vec4)rotationMat.getColumn(0), (Vec4)rotationMat.getColumn(1), (Vec4)rotationMat.getColumn(2), pos);
 
-                Vec3 rotationDir = dir.cross(new Vec3(0,1,0)).getUnitVector();
-                float angle = -dir.angleInRadians(new Vec3(0,1,0));
-                float rndAngle = (float)((new Random()).nextInt(90) * 0.0174532925);
+            int vertexCount = VegetationModel.getVertexArray().length / 3;
+            models = new float[vertexCount * 3];
+            modelsColors = new float[vertexCount * 4];
 
-                Mat4 rotationMat = Matrices.rotate(angle, rotationDir);
-                Mat4 rotationMat2 = Matrices.rotate(rndAngle, dir);
-                rotationMat = rotationMat2.multiply(rotationMat);
-                Mat4 translationMat = new Mat4((Vec4)rotationMat.getColumn(0), (Vec4)rotationMat.getColumn(1), (Vec4)rotationMat.getColumn(2), pos);
+            float scale = 1.5f/(float)Math.pow(2f, GlobalOptions.getInstance().getDetailLevel() + 2f);
+            float heightScale = 1.0f + ((new Random()).nextFloat() / 5f);
 
-                int vertexCount = VegetationModel.getVertexArray().length / 3;
-                models = new float[vertexCount * 3];
-                modelsColors = new float[vertexCount * 4];
+            float rndColor = (new Random()).nextFloat() * 12f / 100f;
 
-                float scale = 1.5f/(float)Math.pow(2f, GlobalOptions.getInstance().getDetailLevel() + 2f);
-                float heightScale = 1.0f + ((new Random()).nextFloat() / 5f);
+            for (int i = 0; i < vertexCount; i++) {
+                float x = VegetationModel.getVertexArray()[i*3 + 0] * scale;
+                float y = VegetationModel.getVertexArray()[i*3 + 1] * scale * heightScale;
+                float z = VegetationModel.getVertexArray()[i*3 + 2] * scale;
+                float w = 1f;
 
-                float rndColor = (new Random()).nextFloat() * 12f / 100f;
+                Vec4 col0 = translationMat.getColumn(0);
+                Vec4 col1 = translationMat.getColumn(1);
+                Vec4 col2 = translationMat.getColumn(2);
+                Vec4 col3 = translationMat.getColumn(3);
 
-                for (int i = 0; i < vertexCount; i++) {
-                    float x = VegetationModel.getVertexArray()[i*3 + 0] * scale;
-                    float y = VegetationModel.getVertexArray()[i*3 + 1] * scale * heightScale;
-                    float z = VegetationModel.getVertexArray()[i*3 + 2] * scale;
-                    float w = 1f;
+                float newX = (col0.getX()*x + col1.getX()*y + col2.getX()*z + col3.getX()*w);
+                float newY = (col0.getY()*x + col1.getY()*y + col2.getY()*z + col3.getY()*w);
+                float newZ = (col0.getZ()*x + col1.getZ()*y + col2.getZ()*z + col3.getZ()*w);
 
-                    Vec4 col0 = translationMat.getColumn(0);
-                    Vec4 col1 = translationMat.getColumn(1);
-                    Vec4 col2 = translationMat.getColumn(2);
-                    Vec4 col3 = translationMat.getColumn(3);
+                models[i*3 + 0] = newX;
+                models[i*3 + 1] = newY;
+                models[i*3 + 2] = newZ;
 
-                    float newX = (col0.getX()*x + col1.getX()*y + col2.getX()*z + col3.getX()*w);
-                    float newY = (col0.getY()*x + col1.getY()*y + col2.getY()*z + col3.getY()*w);
-                    float newZ = (col0.getZ()*x + col1.getZ()*y + col2.getZ()*z + col3.getZ()*w);
-
-                    models[i*3 + 0] = newX;
-                    models[i*3 + 1] = newY;
-                    models[i*3 + 2] = newZ;
-
-                    modelsColors[i*4 + 0] = 0.32f + rndColor;
-                    modelsColors[i*4 + 1] = 0.57f + rndColor;
-                    modelsColors[i*4 + 2] = 0.11f + rndColor;
-                    modelsColors[i*4 + 3] = 1.0f;
-                }
+                modelsColors[i*4 + 0] = 0.32f + rndColor;
+                modelsColors[i*4 + 1] = 0.57f + rndColor;
+                modelsColors[i*4 + 2] = 0.11f + rndColor;
+                modelsColors[i*4 + 3] = 1.0f;
             }
 
 		} else  {
