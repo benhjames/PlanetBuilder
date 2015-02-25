d75f9a788740992cfc821c67f931682995ee2e4a
Alex Bate <alex@alexbate.co.uk>
2015-02-25 16:10:01 +0000

    Put all of #49 back

--- src/uk/ac/cam/cl/bravo/PlanetBuilder/MainClass.java
+++ src/uk/ac/cam/cl/bravo/PlanetBuilder/MainClass.java
@@ -11,6 +11,8 @@ import javax.media.opengl.awt.GLCanvas;
 import javax.media.opengl.awt.GLJPanel;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -33,7 +35,7 @@ public class MainClass {
         GLProfile.initSingleton();
         GLProfile glp = GLProfile.get(GLProfile.GL3);
         GLCapabilities glc = new GLCapabilities(glp);
-        GLCanvas canvas = new GLCanvas(glc);
+        final GLCanvas canvas = new GLCanvas(glc);
         canvas.setSize(canvasWidth, canvasHeight);
 
         try {
@@ -58,17 +60,30 @@ public class MainClass {
             }
         });
         window.setSize(windowWidth, windowHeight);
-        window.setResizable(false);
         window.getContentPane().add(canvas, BorderLayout.CENTER);
         window.getContentPane().add(controls, BorderLayout.EAST);
         window.pack();
+        controlWidth = controls.getWidth();
+        window.setResizable(true);
         window.setVisible(true);
+        window.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
+                int newWidth = window.getWidth();
+                canvas.setSize(newWidth - controlWidth, window.getHeight());
+            }
+        });
 
         MainWindow mainWindow = new MainWindow();
 
 		KeyInput.setWindow(mainWindow);
         Leap.setWindow(mainWindow);
         canvas.addKeyListener(new KeyInput());
+
+        while(!canvas.areAllGLEventListenerInitialized()) {
+            			//Have to wait to initialise animator otherwise blank screens!
+                    	    }
 
         canvas.addGLEventListener(mainWindow);
 
